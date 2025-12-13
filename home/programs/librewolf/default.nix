f2a26792e4b5061888a0f6c87d7a51a27f686511
lunarmelon <lunarmelon@tuta.io>
2025-12-13 02:13:14 -0600

    fix(librewolf): simplify configuration

--- home/programs/librewolf/default.nix
+++ home/programs/librewolf/default.nix
@@ -3,8 +3,6 @@
     enable = true;
     settings = {
       "privacy.clearOnShutdown.history" = false;
-      "browser.newtabpage.activity-stream.trendingSearch.defaultSearchEngine" = "StartPage";
-      "browser.policies.runOncePerModification.setDefaultSearchEngine" = "StartPage";
       "identity.fxaccounts.enabled" = true;
     };
     profiles = {
@@ -12,27 +10,6 @@
         id = 0;
         name = "default";
         path = "default";
-        search = {
-          force = true;
-        };
-        extensions = {
-          force = true;
-          packages = with pkgs.nur.repos.rycee.firefox-addons; [ublock-origin decentraleyes];
-          settings."uBlock0@raymondhill.net".settings = {
-            selectedFilterLists = [
-              "ublock-filters"
-              "ublock-badware"
-              "ublock-privacy"
-              "ublock-unbreak"
-              "ublock-quick-fixes"
-            ];
-          };
-        };
-      };
-      "melon" = {
-        id = 1;
-        name = "melon";
-        path = "melon";
       };
     };
   };
