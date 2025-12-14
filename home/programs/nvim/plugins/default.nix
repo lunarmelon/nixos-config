8d5cad5d7fc6a120b48bb1ef62d3bb047fcacde9
lunarmelon <lunarmelon@tuta.io>
2025-12-14 13:19:31 -0600

    feat(nvim): add typescript tools

--- home/programs/nvim/plugins/default.nix
+++ home/programs/nvim/plugins/default.nix
@@ -13,5 +13,6 @@
     ./noice
     ./todo-comments
     ./treesitter
+    ./typescript-tools
   ];
 }
---
+++ home/programs/nvim/plugins/typescript-tools/default.nix
@@ -0,0 +1,11 @@
+{
+  plugins.typescript-tools = {
+    enable = true;
+    settings = {
+      tsserver_format_options = {
+        indentSize = 4;
+        convertTabsToSpaces = true;
+      };
+    };
+  };
+}
