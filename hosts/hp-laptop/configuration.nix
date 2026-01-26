93f036b11f35ebfbb5047214e81543b797c99b49
lunarmelon <lunarmelon@tuta.io>
2026-01-25 22:21:55 -0600

    chore(nixos): clean host config file

--- hosts/hp-laptop/configuration.nix
+++ hosts/hp-laptop/configuration.nix
@@ -1,11 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -104,21 +97,6 @@
   ];
 
   environment.pathsToLink = ["/share/xdg-desktop-portal" "/share/applications"];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
