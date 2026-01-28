528163396ff2a65f219440306b240d3c3d029356
lunarmelon <lunarmelon@tuta.io>
2026-01-28 17:09:29 -0600

    feat(nvim): add neovim options

--- home-modules/nvim/default.nix
+++ home-modules/nvim/default.nix
@@ -1,10 +1,23 @@
 {
-  programs.nixvim = {
-    enable = true;
-    imports = [
-      ./core
-      ./lsp
-      ./plugins
-    ];
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.userSettings.nvim;
+in {
+  options = {
+    userSettings.nvim = {
+      enable = lib.mkEnableOption "Enable neovim";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    programs.nixvim = {
+      enable = true;
+      imports = [
+        ./core
+        ./lsp
+        ./plugins
+      ];
+    };
   };
 }
