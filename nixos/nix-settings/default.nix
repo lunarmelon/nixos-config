5ade4455c28a633e58d72f87b891b8ba3c025103
lunarmelon <lunarmelon@tuta.io>
2026-01-27 18:41:57 -0600

    feat(nixos): add substituters for prebuilt binaries

--- nixos/nix-settings/default.nix
+++ nixos/nix-settings/default.nix
@@ -9,6 +9,16 @@
       "nixos-test"
     ];
 
+    # Substituters
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
     # Enable flakes
     experimental-features = [
       "nix-command"
@@ -16,12 +26,15 @@
     ];
     cores = 0;
     auto-optimise-store = true; # enable space optimisation by hardlinking
+    trusted-users = ["@wheel"]; # wheel group gets trusted access to nix daemon
   };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
-
-  environment.pathsToLink = ["/share/xdg-desktop-portal" "/share/applications"];
+  environment = {
+    sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
+    pathsToLink = ["/share/xdg-desktop-portal" "/share/applications"];
+  };
 
   system.stateVersion = "25.05";
 }
