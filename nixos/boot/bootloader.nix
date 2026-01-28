e75981aa4a94c2f8a0c3777eea5e232ef5647417
lunarmelon <lunarmelon@tuta.io>
2026-01-27 18:36:26 -0600

    feat(nixos): add kernel boot params

--- nixos/boot/bootloader.nix
+++ nixos/boot/bootloader.nix
@@ -1,5 +1,27 @@
 # This config file is used to configure the bootloader
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    # Silent Boot
+    # https://wiki.archlinux.org/title/Silent_boot
+    kernelParams = [
+      "quiet"
+      "splash"
+      "vga=current"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    initrd = {
+      systemd.enable = true;
+      verbose = false;
+    };
+    plymouth.enable = false;
+  };
 }
