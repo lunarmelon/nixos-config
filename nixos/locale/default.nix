81697f65fcba93f93d6de48afd3dc3c55ca76ab9
lunarmelon <lunarmelon@tuta.io>
2026-01-27 18:45:31 -0600

    feat(nixos): add system clock synchronization

--- nixos/locale/default.nix
+++ nixos/locale/default.nix
@@ -2,6 +2,9 @@
   # Set your time zone.
   time.timeZone = "America/Mexico_City";
 
+  # Synchronize system clock
+  services.timesyncd.enable = true;
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
