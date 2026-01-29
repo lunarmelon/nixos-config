af4cb4d2ad5a29c02dfd2c51c7891545a0da508a
lunarmelon <lunarmelon@tuta.io>
2026-01-28 18:10:12 -0600

    feat(nixos): add acer nitro system config

---
+++ hosts/lain-nitro/configuration.nix
@@ -0,0 +1,29 @@
+{
+  config = {
+    systemSettings = {
+      # Users
+      users = ["melon"];
+      adminUsers = ["melon"];
+
+      # Hardware
+      kernel.enable = true;
+      kernel.variant = "cachy";
+      bluetooth.enable = true;
+      printing.enable = true;
+
+      # Software
+      flatpak.enable = true;
+      plasma.enable = true;
+      prism-launcher.enable = true;
+      steam.enable = true;
+
+      # Security
+      security = {
+        automount.enable = true;
+        #firejail.enable = true;
+        firewall.enable = true;
+      };
+    };
+    users.users.melon.description = "Melon";
+  };
+}
