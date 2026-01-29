19f6bc55275839bf774ba29d9b583dabaf1253ca
lunarmelon <lunarmelon@tuta.io>
2026-01-28 18:13:48 -0600

    feat(nixos): add configs for lain-nitro

---
+++ hosts/lain-nitro/default.nix
@@ -0,0 +1,16 @@
+{config, ...}: {
+  imports = [
+    ./configuration.nix
+    #./hardware-configuration.nix
+  ];
+
+  config = {
+    home-manager.users =
+      builtins.listToAttrs
+      (map (user: {
+          name = user;
+          value = {imports = [./home.nix ../../home-modules];};
+        })
+        config.systemSettings.users);
+  };
+}
---
+++ hosts/lain-nitro/home.nix
@@ -0,0 +1,25 @@
+{
+  config = {
+    userSettings = {
+      # Shell
+      zsh.enable = true;
+
+      # Programs
+      chromium.enable = true;
+      fastfetch.enable = true;
+      git.enable = true;
+      gpg.enable = true;
+      keepassxc.enable = true;
+      lazygit.enable = true;
+      librewolf.enable = true;
+      nvim.enable = true;
+      oh-my-posh.enable = true;
+
+      # Programming
+      programming = {
+        bun.enable = true;
+        npm.enable = true;
+      };
+    };
+  };
+}
