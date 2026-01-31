c81332edd8edde7b3bfb506cce784d3c52daed9e
lunarmelon <lunarmelon@tuta.io>
2026-01-30 22:27:02 -0600

    feat(nixos-programming): add utilities

--- nixos-modules/programming/default.nix
+++ nixos-modules/programming/default.nix
@@ -13,12 +13,15 @@ in {
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
+      cargo
       dotnet-sdk_10
       go
       libgcc
       lua
+      luarocks
       perl
       python3
+      python313Packages.pip
       rustc
     ];
   };
