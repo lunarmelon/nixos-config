d62d73f6cc25338cf6f2224dab666e86a6bed5d0
lunarmelon <lunarmelon@tuta.io>
2026-01-31 23:20:14 -0600

    feat(nixos-services): enable services

--- nixos-modules/services/default.nix
+++ nixos-modules/services/default.nix
@@ -2,5 +2,14 @@
   services = {
     smartd.enable = true;
     fwupd.enable = true; # For firmware updates
+
+    # Accounts daemon is needed to remember passwords and other account information
+    # by display manager and other services
+    accounts-daemon.enable = true;
+    dbus = {
+      enable = true;
+      implementation = "broker"; # use new dbus-broker
+    };
+    udev.enable = true;
   };
 }
