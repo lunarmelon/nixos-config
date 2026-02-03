c64380ee8ad1bae1ef689ad0155ab393dc0e7229
lunarmelon <lunarmelon@tuta.io>
2026-02-03 12:13:53 -0600

    feat(nixos-dwm): add catppuccin colors

--- nixos-modules/wm/dwm/dwm/config.def.h
+++ nixos-modules/wm/dwm/dwm/config.def.h
@@ -13,36 +13,61 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"Fira Sans:size=11"};
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=11"};
 static const char dmenufont[] = "JetBrainsMono Nerd Font:size=10";
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
-static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#f5c2e7";
+
+/* Catppuccin Mocha colors */
+static const char col_rosewater[] = "#f5e0dc";
+static const char col_flamingo[] = "#f2cdcd";
+static const char col_pink[] = "#f5c2e7";
+static const char col_mauve[] = "#cba6f7";
+static const char col_red[] = "#f38ba8";
+static const char col_maroon[] = "#eba0ac";
+static const char col_peach[] = "#fab387";
+static const char col_yellow[] = "#f9e2af";
+static const char col_green[] = "#a6e3a1";
+static const char col_teal[] = "#94e2d5";
+static const char col_sky[] = "#89dceb";
+static const char col_sapphire[] = "#74c7ec";
+static const char col_blue[] = "#89b4fa";
+static const char col_lavender[] = "#b4befe";
+static const char col_text[] = "#cdd6f4";
+static const char col_subtext1[] = "#bac2de";
+static const char col_subtext0[] = "#a6adc8";
+static const char col_overlay2[] = "#9399b2";
+static const char col_overlay1[] = "#7f849c";
+static const char col_overlay0[] = "#6c7086";
+static const char col_surface2[] = "#585b70";
+static const char col_surface1[] = "#45475a";
+static const char col_surface0[] = "#313244";
+static const char col_base[] = "#1e1e2e";
+static const char col_mantle[] = "#181825";
+static const char col_crust[] = "#11111b";
+
+static const char custom_color1[] = "#875fff";
+
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
-    [SchemeStatus] = {col_gray3, col_gray1,
-                      "#000000"}, // Statusbar right {text,background,not used
+    [SchemeNorm] = {col_overlay0, col_base, col_surface0},
+    [SchemeSel] = {col_text, custom_color1, custom_color1},
+    [SchemeStatus] = {col_text, col_crust,
+                      col_crust}, // Statusbar right {text,background,not used
                                   // but cannot be empty}
-    [SchemeTagsSel] = {col_gray4, col_cyan,
-                       "#000000"}, // Tagbar left selected {text,background,not
+    [SchemeTagsSel] = {col_text, custom_color1,
+                       col_crust}, // Tagbar left selected {text,background,not
                                    // used but cannot be empty}
     [SchemeTagsNorm] =
-        {col_gray3, col_gray1,
-         "#000000"}, // Tagbar left unselected {text,background,not used but
+        {col_text, col_crust,
+         col_crust}, // Tagbar left unselected {text,background,not used but
                      // cannot be empty}
     [SchemeInfoSel] =
-        {col_gray4, col_cyan,
-         "#000000"}, // infobar middle  selected {text,background,not used but
+        {col_text, col_crust,
+         col_crust}, // infobar middle  selected {text,background,not used but
                      // cannot be empty}
     [SchemeInfoNorm] =
-        {col_gray3, col_gray1,
-         "#000000"}, // infobar middle  unselected {text,background,not used but
-                     // cannot be empty}
-
+        {col_text, col_crust,
+         col_crust}, // infobar middle  unselected {text,background,not
+                     // used but cannot be empty}
 };
 
 /* tagging */
@@ -54,8 +79,7 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
+    {"Kitty", NULL, NULL, 0, 1, -1},
 };
 
 /* layout(s) */
@@ -107,9 +131,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+static const char *dmenucmd[] = {"dmenu_run", "-m",  dmenumon,      "-fn",
+                                 dmenufont,   "-nb", col_gray1,     "-nf",
+                                 col_gray3,   "-sb", custom_color1, "-sf",
+                                 col_gray4,   NULL};
 static const char *termcmd[] = {"kitty", NULL};
 
 #define BROWSER "librewolf"
