bb5969a6b294c37c9fd34db5a67153335495b614
lunarmelon <lunarmelon@tuta.io>
2026-02-04 21:03:26 -0600

    feat(home-wleave): add wleave action buttons

--- home-modules/wleave/default.nix
+++ home-modules/wleave/default.nix
@@ -16,6 +16,7 @@ in {
     #   enable = true;
     #   iconStyle = "wlogout";
     # };
+    home.packages = [pkgs.wayland-logout];
     programs.wleave = {
       enable = true;
       settings = {
@@ -26,6 +27,20 @@ in {
             text = "Lock";
             keybind = "l";
             icon = "${pkgs.wleave}/share/wleave/icons/lock.svg";
+          }
+          {
+            label = "logout";
+            action = "wayland-logout";
+            text = "Logout";
+            keybind = "e";
+            icon = "${pkgs.wleave}/share/wleave/icons/logout.svg";
+          }
+          {
+            label = "shutdown";
+            action = "systemctl poweroff";
+            text = "Shutdown";
+            keybind = "s";
+            icon = "${pkgs.wleave}/share/wleave/icons/shutdown.svg";
           }
         ];
       };
