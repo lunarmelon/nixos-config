ce6788d9cea78629087ed11a6b355a46a05545f1
lunarmelon <lunarmelon@tuta.io>
2026-02-04 22:29:03 -0600

    feat(home-swaylock): add swaylock-effects

--- home-modules/swaylock/default.nix
+++ home-modules/swaylock/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.userSettings.swaylock;
@@ -14,6 +15,24 @@ in {
     catppuccin.swaylock.enable = true;
     programs.swaylock = {
       enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        image = ./phos_head.jpg;
+        font = "JetBrainsMono Nerd Font";
+        font-size = 44;
+        indicator-radius = 200;
+        show-failed-attempts = true;
+        ignore-empty-password = true;
+
+        # swaylock-effects specific
+        clock = true;
+        indicator = true;
+        timestr = "%H:%M";
+        datestr = "%a %d-%b";
+        effect-blur = "7x5";
+        effect-vignette = "0.5:0.5";
+        grace = 2;
+      };
     };
   };
 }
