fb59bb2937ae92598fbf0e7e6741597aba11f6f6
lunarmelon <lunarmelon@tuta.io>
2026-02-04 18:48:13 -0600

    feat(home-wlogout): add wlogout options

--- home-modules/wlogout/default.nix
+++ home-modules/wlogout/default.nix
@@ -1,5 +1,18 @@
 {
-  programs.wlogout = {
-    enable = true;
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.userSettings.wlogout;
+in {
+  options = {
+    userSettings.wlogout = {
+      enable = lib.mkEnableOption "Enable wlogout";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    programs.wlogout = {
+      enable = true;
+    };
   };
 }
