4becd1a24cf5e46b2c147c393c4f3efcbcbd33c4
lunarmelon <lunarmelon@tuta.io>
2026-02-08 00:33:29 -0600

    feat(home-obs): add pipewire audio support for obs

--- home-modules/obs/default.nix
+++ home-modules/obs/default.nix
@@ -15,6 +15,7 @@ in {
     programs.obs-studio.enable = true;
     programs.obs-studio.plugins = with pkgs.obs-studio-plugins; [
       obs-gstreamer
+      obs-pipewire-audio-capture
       obs-vaapi
       wlrobs
     ];
