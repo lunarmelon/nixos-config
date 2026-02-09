b4cd3ab62546429aed2f8cae5b84a5d4fa171647
lunarmelon <lunarmelon@tuta.io>
2026-02-08 19:43:09 -0600

    feat(home-mpd): add music player daemon

---
+++ home-modules/mpd/default.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.userSettings.mpd;
+in {
+  options = {
+    userSettings.mpd = {
+      enable = lib.mkEnableOption "Enable mpd";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    # activate the service
+    services.mpd = {
+      enable = true;
+      network.startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+      extraConfig = ''
+        audio_output {
+          type        "pulse"
+          name        "Pulse Audio"
+          mixer_type  "software"
+        }
+      '';
+    };
+  };
+}
